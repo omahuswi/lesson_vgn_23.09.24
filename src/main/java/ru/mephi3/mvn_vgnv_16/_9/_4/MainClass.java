9d453348af6a3495d0c177c1e81c0ae27ffb8b65
Vlada Davydova <davudovavlada@gmail.com>
2024-09-23 13:07:32 +0500

    init

---
+++ src/main/java/ru/mephi3/mvn_vgnv_16/_9/_4/MainClass.java
@@ -0,0 +1,7 @@
+package ru.mephi3.mvn_vgnv_16._9._4;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("aaaa");
+    }
+}
---
+++ src/test/java/ru/mephi3/mvn_vgnv_16/_9/_4/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mephi3.mvn_vgnv_16._9._4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
